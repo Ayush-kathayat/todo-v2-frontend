9edbf0588768d6ce868cb751bce5714c0b04f800
Ayush-kathayat <kathayatayush789@gmail.com>
2024-04-11 14:20:44 +0530

    chore/register : added css-transition-group for smooth transistion between the register and the login form

--- src/Pages/Landing/landing.tsx
+++ src/Pages/Landing/landing.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
+
 import Register from "../../components/Auth/register";
+import Login from "../../components/Auth/login";
 
 import "./landing.css";
 const Landing = () => {
+  const [isLoginForm, setIsLoginForm] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsLoginForm(true);
+  };
+
+  const handleRegisterClick = () => {
+    setIsLoginForm(false);
+  };
+
   return (
     <div className="landing-wrapper">
       <div className="land-left">
@@ -16,7 +30,33 @@ const Landing = () => {
             Elevate Your Productivity with Our Dynamic Todo App{" "}
           </p>{" "}
         </div>
-        <Register />
+        <CSSTransition
+          in={isLoginForm}
+          timeout={500}
+          classNames="fade"
+          unmountOnExit
+        >
+          <Login />
+        </CSSTransition>
+        <CSSTransition
+          in={!isLoginForm}
+          timeout={500}
+          classNames="fade"
+          unmountOnExit
+        >
+          <Register />
+        </CSSTransition>
+        <div className="login-register">
+          {isLoginForm ? (
+            <p className="switch-form-link" onClick={handleRegisterClick}>
+              Don't have an account?
+            </p>
+          ) : (
+            <p className="switch-form-link" onClick={handleLoginClick}>
+              Already have an account?
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Auth/login.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="login-wrapper">
+      <h1> hello login here!</h1>
+    </div>
+  );
+}
+
+export default Login;
